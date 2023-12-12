06f7603a6a462001052c668f069a77e173f05eda
章昊男 <zhn.pop@gmail.com>
2023-12-12 15:03:52 +0800

    final

--- infrastructure-kit/src/main/java/com/holland/infrastructure/kit/kit/DateKit.java
+++ infrastructure-kit/src/main/java/com/holland/infrastructure/kit/kit/DateKit.java
@@ -4,19 +4,19 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.Temporal;
 
 public class DateKit {
-    public static String YYYY = "yyyy";
-    public static String YYYY_MM = "yyyy-MM";
-    public static String YYYY_MM_DD = "yyyy-MM-dd";
-    public static String HH_MM_SS = "HH:mm:ss";
-    public static String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
-    public static String YYYYMMDDHH = "yyyyMMddHH";
-    public static String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    public static final String YYYY = "yyyy";
+    public static final String YYYY_MM = "yyyy-MM";
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+    public static final String HH_MM_SS = "HH:mm:ss";
+    public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+    public static final String YYYYMMDDHH = "yyyyMMddHH";
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
-    public static DateTimeFormatter DTF_YYYY_MM_DD = DateTimeFormatter.ofPattern(YYYY_MM_DD);
-    public static DateTimeFormatter DTF_HH_MM_SS = DateTimeFormatter.ofPattern(HH_MM_SS);
-    public static DateTimeFormatter DTF_YYYYMMDDHHMMSS = DateTimeFormatter.ofPattern(YYYYMMDDHHMMSS);
-    public static DateTimeFormatter DTF_YYYYMMDDHH = DateTimeFormatter.ofPattern(YYYYMMDDHH);
-    public static DateTimeFormatter DTF_YYYY_MM_DD_HH_MM_SS = DateTimeFormatter.ofPattern(YYYY_MM_DD_HH_MM_SS);
+    public static final DateTimeFormatter DTF_YYYY_MM_DD = DateTimeFormatter.ofPattern(YYYY_MM_DD);
+    public static final DateTimeFormatter DTF_HH_MM_SS = DateTimeFormatter.ofPattern(HH_MM_SS);
+    public static final DateTimeFormatter DTF_YYYYMMDDHHMMSS = DateTimeFormatter.ofPattern(YYYYMMDDHHMMSS);
+    public static final DateTimeFormatter DTF_YYYYMMDDHH = DateTimeFormatter.ofPattern(YYYYMMDDHH);
+    public static final DateTimeFormatter DTF_YYYY_MM_DD_HH_MM_SS = DateTimeFormatter.ofPattern(YYYY_MM_DD_HH_MM_SS);
 
     public static String format(Temporal temporal, DateTimeFormatter formatter) {
         return formatter.format(temporal);
